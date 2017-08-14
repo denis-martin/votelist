728536e1dcaecca24070a673910801d7fbfc1f26
denis <denis@misc-net.de>
2017-08-14 22:39:02 +0200

    added DB schema

---
+++ server/db/votelist-schema.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 5.5.57, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: denis_td_votelist
+-- ------------------------------------------------------
+-- Server version	5.5.57-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `denis_td_votelist`
+--
+
+DROP TABLE IF EXISTS `denis_td_votelist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `denis_td_votelist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ipAddress` varchar(40) DEFAULT NULL,
+  `guid` varchar(40) DEFAULT NULL,
+  `changedAt` datetime DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  `votesUp` int(11) NOT NULL DEFAULT '0',
+  `votesDown` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `denis_td_votes`
+--
+
+DROP TABLE IF EXISTS `denis_td_votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `denis_td_votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vlId` int(11) NOT NULL,
+  `guid` varchar(40) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vlIdGuid` (`vlId`,`guid`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-14 22:34:37
